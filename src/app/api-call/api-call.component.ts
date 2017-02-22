639e0482102badd7eb9c1d3fdb82cf3a223f4c53
hassan-ali-10p <hassan.ali@tenpearls.com>
2017-02-22 18:25:39 +0500

    Loader added

--- src/app/api-call/api-call.component.ts
+++ src/app/api-call/api-call.component.ts
@@ -12,6 +12,7 @@ export class ApiCallComponent implements OnInit {
   apiCall: IApiCall;
   token: string;
   response: any;
+  isLoading: boolean = false;
 
   constructor(private apiCallService: ApiCallService) { }
 
@@ -27,10 +28,14 @@ export class ApiCallComponent implements OnInit {
   }
 
   patientSearch() {
-    this.apiCallService.patientSearch(this.apiCall.params).then((result) => {this.response = result});
+    this.apiCallService.patientSearch(this.apiCall.params).then((result) => {
+      this.isLoading = false;
+      return this.response = result;
+    });
   }
 
   onSubmit():void {
+    this.isLoading = true;
     this.patientSearch();
   }
 
