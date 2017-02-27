efac8eef44423939f0f230847737c28ea1e11dc1
hassan-ali-10p <hassan.ali@tenpearls.com>
2017-02-27 12:09:33 +0500

    app title changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EMR Api Doc';
+  title = 'Blackwell Gateway Testing Tool';
 }
